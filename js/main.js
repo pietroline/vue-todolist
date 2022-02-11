dc4456a474ff0544aa557ea7d011e32c8de80589
pietroline <pietrobarberi@hotmail.it>
2022-02-11 15:50:20 +0100

    milestone 1

--- js/main.js
+++ js/main.js
@@ -3,11 +3,30 @@ const vue = new Vue (
     {
         el: "#app",        
         data: {
-          
+          todos:[
+            {
+                text: "studiare javascript",
+                done: false,
+                color: "#c82a0e",
+            },
+            {
+                text: "mangiare",
+                done: true,
+                color: "#c82a0e",
+            },{
+                text: "dormire",
+                done: true,
+                color: "#c82a0e",
+            },{
+                text: "spesa",
+                done: false,
+                color: "#c82a0e",
+            }
+          ],
         },
 
         methods:{
-          
+         
          
         }
     }
